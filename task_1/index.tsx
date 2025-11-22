39d4f193aa46ff685426c9f200b0d0ee49273545
Mona Magomedova <64916820+MonaMag@users.noreply.github.com>
2025-11-22 17:01:21 +0300

    Update index.tsx for task_1

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -10,22 +10,29 @@ type IProps = {
 }
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
+const FirstComponent = React.memo(({ name, age }: IUser) => (
     <div>
         my name is {name}, my age is {age}
     </div>
-);
+));
 
 // functional component
 // Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
+const SecondComponentBase = ({ user: { name, age } }: IProps) => (
     <div>
         my name is {name}, my age is {age}
     </div>
 );
 
+function propsAreEqual(prev: IProps, next: IProps) {
+    return prev.user.name === next.user.name &&
+           prev.user.age === next.user.age;
+}
+
+export const SecondComponent = React.memo(SecondComponentBase, propsAreEqual);
+
 // class component
-class ThirdComponent extends Component<IUser> {
+class ThirdComponent extends React.PureComponent<IUser> {
     render() {
         return (
             <div>
@@ -37,6 +44,12 @@ class ThirdComponent extends Component<IUser> {
 
 // class component
 class FourthComponent extends Component<IProps> {
+    shouldComponentUpdate(nextProps: IProps) {
+        return (
+            this.props.user.name !== nextProps.user.name ||
+            this.props.user.age !== nextProps.user.age
+        );
+    }
     render() {
         return (
             <div>
